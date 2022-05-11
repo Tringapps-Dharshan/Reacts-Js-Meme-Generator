025ca21ef0b2ef154a6aa4aea6ffa589b30f8faf
Tringapps-Dharshan <102800724+Tringapps-Dharshan@users.noreply.github.com>
2022-05-11 12:28:09 +0530

    Upload

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import Header from './Component/Header';
+import Meme from './Component/Meme';
+import './App.css';
+
+function App() {
+  return (
+    <div className='content'>
+      <Header/>
+      <Meme/> 
+    </div>
+  );
+}
+
+export default App;
